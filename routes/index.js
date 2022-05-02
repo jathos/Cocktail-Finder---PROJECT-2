ac8a8019da792b2efaaef60c028916d7837a20da
jbellon <josh@joshbellon.com>
2022-05-02 14:01:12 -0700

    node-fetch is installed and mounted on current routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
