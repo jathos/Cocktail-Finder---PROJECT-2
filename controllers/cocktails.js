f950647b1699aaa83c688e55e33d7aa158b1c545
jbellon <josh@joshbellon.com>
2022-05-02 16:21:12 -0700

    All cocktails can now be browsed by first letter, and results are shown with a picture, the cocktail name, and a short description

--- controllers/cocktails.js
+++ controllers/cocktails.js
@@ -5,11 +5,245 @@ const token = process.env.COCKTAILDB_TOKEN;
 const rootURL = 'https://www.thecocktaildb.com/api/json/v1/';
 
 module.exports = {
-    showAll
+    showAll,
+    showA,
+    showB,
+    showC,
+    showD,
+    showE,
+    showF,
+    showG,
+    showH,
+    showI,
+    showJ,
+    showK,
+    showL,
+    showM,
+    showN,
+    showO,
+    showP,
+    showQ,
+    showR,
+    showS,
+    showT,
+    showU,
+    showV,
+    showW,
+    showX,
+    showY,
+    showZ
 };
 
 function showAll(req, res) {
     fetch(`${rootURL}${token}/random.php`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showA(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=a`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showB(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=b`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showC(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=c`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showD(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=d`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showE(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=e`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showF(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=f`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showG(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=g`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showH(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=h`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showI(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=i`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showJ(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=j`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showK(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=a`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showL(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=l`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showM(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=m`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showN(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=n`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showO(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=o`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showP(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=p`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showQ(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=q`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showR(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=r`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showS(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=s`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showT(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=t`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showU(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=u`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showV(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=v`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showW(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=w`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showX(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=x`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showY(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=y`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
+};
+
+function showZ(req, res) {
+    fetch(`${rootURL}${token}/search.php?f=z`)
         .then(res => res.json())
         .then(cocktailData => {
             res.render('cocktails/show', { cocktailData });
--- routes/cocktails.js
+++ routes/cocktails.js
@@ -5,4 +5,31 @@ const cocktailsCtrl = require('../controllers/cocktails');
 
 router.get('/', cocktailsCtrl.showAll);
 
+router.get('/a', cocktailsCtrl.showA);
+router.get('/b', cocktailsCtrl.showB);
+router.get('/c', cocktailsCtrl.showC);
+router.get('/d', cocktailsCtrl.showD);
+router.get('/e', cocktailsCtrl.showE);
+router.get('/f', cocktailsCtrl.showF);
+router.get('/g', cocktailsCtrl.showG);
+router.get('/h', cocktailsCtrl.showH);
+router.get('/i', cocktailsCtrl.showI);
+router.get('/j', cocktailsCtrl.showJ);
+router.get('/k', cocktailsCtrl.showK);
+router.get('/l', cocktailsCtrl.showL);
+router.get('/m', cocktailsCtrl.showM);
+router.get('/n', cocktailsCtrl.showN);
+router.get('/o', cocktailsCtrl.showO);
+router.get('/p', cocktailsCtrl.showP);
+router.get('/q', cocktailsCtrl.showQ);
+router.get('/r', cocktailsCtrl.showR);
+router.get('/s', cocktailsCtrl.showS);
+router.get('/t', cocktailsCtrl.showT);
+router.get('/u', cocktailsCtrl.showU);
+router.get('/v', cocktailsCtrl.showV);
+router.get('/w', cocktailsCtrl.showW);
+router.get('/x', cocktailsCtrl.showX);
+router.get('/y', cocktailsCtrl.showY);
+router.get('/z', cocktailsCtrl.showZ);
+
 module.exports = router;
