9ae6a3961adcc364b1006b95ab7f57f40653ed38
jbellon <josh@joshbellon.com>
2022-05-02 14:38:37 -0700

    Node-fetch is now correctly mounted as required by current version.  A function exists that will show a JSON for a random cocktail from the API by clicking on the <browse all cocktails> link

--- controllers/cocktails.js
+++ controllers/cocktails.js
@@ -1,9 +1,17 @@
 //const Cocktail = STILL NEED A PATH TO THE MODEL HERE
+const fetch = (...args) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const token = process.env.COCKTAILDB_TOKEN;
+const rootURL = 'https://www.thecocktaildb.com/api/json/v1/';
 
 module.exports = {
     showAll
 };
 
 function showAll(req, res) {
-    res.render('cocktails/show')
+    fetch(`${rootURL}${token}/random.php`)
+        .then(res => res.json())
+        .then(cocktailData => {
+            res.render('cocktails/show', { cocktailData });
+        });
 };
