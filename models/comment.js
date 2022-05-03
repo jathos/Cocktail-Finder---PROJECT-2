e01736e48ccd2f05533b22449c83c230db133c7b
jbellon <josh@joshbellon.com>
2022-05-03 14:57:30 -0700

    Oauth is set up, passport is installed and initialized in config/passport, and oauth routes have been added to the index router.

---
+++ models/comment.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    text: String
+}, {
+    timestamps: true
+});
+
+
+module.exports = mongoose.model('Comment', commentSchema);
