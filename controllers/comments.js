feec7f484574e71e6459452a15afeee778932a0c
jbellon <josh@joshbellon.com>
2022-05-04 14:11:26 -0700

    MVP present.  Full CRUD is now possible.  Comments can now be deleted by the logged in user who created them.

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,7 +1,9 @@
+const { redirect } = require('express/lib/response');
 const Comment = require('../models/comment');
 
 module.exports = {
-    create
+    create,
+    deleteComment
 };
 
 function create(req, res) {
@@ -12,4 +14,19 @@ function create(req, res) {
     Comment.create(req.body, function (err, comment) {
         res.redirect(`/cocktails/${req.params.id}`)
     })
+};
+
+function deleteComment(req, res, next) {
+    let redirectId
+    Comment.findById(req.params.id)
+        .then(function (doc) {
+            if (!doc.user.equals(req.user._id)) return res.redirect('/');
+            redirectId = doc.cocktailId;
+            doc.remove()
+                .then(function () {
+                    res.redirect(`/cocktails/${redirectId}`);
+                }).catch(function (err) {
+                    return next(err);
+                });
+        });
 };
--- routes/comments.js
+++ routes/comments.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const commentsCtrl = require('../controllers/comments');
 
 router.post('/:id', commentsCtrl.create);
+router.delete('/:id', commentsCtrl.deleteComment);
 
 module.exports = router;
